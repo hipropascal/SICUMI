dce5182c26343612cc1d316777152aab6120e4cc
hipropascal <hendriprayugo@gmail.com>
2018-07-16 18:07:48 +0700

    Make sure
    s

--- src/db/dbscheme.py
+++ src/db/dbscheme.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
+
 class RasterUpdatePackage(Base):
     __tablename__ = 'RASTER_UPDATE_PACKAGE'
     id = Column(Integer, primary_key=True)
